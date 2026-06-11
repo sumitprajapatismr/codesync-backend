31639fd67b451bfd1edbc9177899ea2665edc822
Sumit Prajapati <prajapatisumitop@gmail.com>
2026-06-11 10:38:47 +0530

    demo run code

--- src/utils/piston.js
+++ src/utils/piston.js
@@ -1,44 +1,9 @@
-const axios = require("axios");
-
-const languageMap = {
-  javascript: 63,
-  python: 71,
-  cpp: 54,
-  java: 62,
-};
-
 const executeCode = async (language, code, stdin = "") => {
-  try {
-    const res = await axios.post(
-      "https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=true",
-      {
-        source_code: code,
-        language_id: languageMap[language],
-        stdin: stdin || "",
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-RapidAPI-Key": "YOUR_RAPIDAPI_KEY",
-          "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-        },
-      }
-    );
-
-    return {
-      stdout: res.data.stdout,
-      stderr: res.data.stderr,
-      code: res.data.status?.id,
-    };
-  } catch (err) {
-    console.log("EXEC ERROR:", err.response?.data || err.message);
-
-    return {
-      stdout: "",
-      stderr: "Execution Failed",
-      code: 1,
-    };
-  }
+  return {
+    stdout: "Code executed successfully",
+    stderr: "",
+    code: 0,
+  };
 };
 
 module.exports = executeCode;
